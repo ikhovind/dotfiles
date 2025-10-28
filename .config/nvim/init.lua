01d44121878c755ec02a50712d967a0ef6b0e30d
Ingebrigt Hovind <ing.hovind@hotmail.com>
2025-10-28 16:40:15 +0100

    Add tmux etc

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -340,12 +340,25 @@ pcall(require('telescope').load_extension, 'projects')
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim' },
+  ensure_installed = { 
+    'c',
+    'cpp',
+    'go',
+    'lua',
+    'python',
+    'rust',
+    'tsx',
+    'typescript',
+    'vimdoc',
+    'vim', 
+    "luadoc",
+    "markdown"
+  },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = false,
 
-  highlight = { enable = true },
+  highlight = { enable = true, disable = "help" },
   indent = { enable = true },
   incremental_selection = {
     enable = true,
